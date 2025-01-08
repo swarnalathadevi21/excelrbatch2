1aacd4f9d1346e95ede97345028b40a268a6d68d
swarnalathadevi21 <swarnalathadevi94@gmail.com>
2025-01-08 09:54:41 +0530

    Update DisplayNumbers.java

--- DisplayNumbers.java
+++ DisplayNumbers.java
@@ -1,6 +1,5 @@
 public class DisplayNumbers {
     public static void main(String[] args) {
-        // For loop to display numbers from 1 to 100
         for (int i = 1; i <= 100; i++) {
             System.out.println(i);
         }
