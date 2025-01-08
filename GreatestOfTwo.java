de57aa350ed377814413060965f67429c7ae5d5e
swarnalathadevi21 <swarnalathadevi94@gmail.com>
2025-01-08 09:55:56 +0530

    Update GreatestOfTwo.java

--- GreatestOfTwo.java
+++ GreatestOfTwo.java
@@ -1,10 +1,7 @@
 public class GreatestOfTwo {
     public static void main(String[] args) {
-        // Assign two numbers
         int num1 = 25;
         int num2 = 40;
-
-        // Compare the numbers
         if (num1 > num2) {
             System.out.println(num1 + " is the greatest.");
         } else if (num2 > num1) {
